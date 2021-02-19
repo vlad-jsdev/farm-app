8362f1cf2e24c6b21018df660467b2f2ca2a7643
vladosxxx <vladosxxx@gmail.com>
2021-02-19 18:47:06 +0200

    almost done

--- src/components/Finish.vue
+++ src/components/Finish.vue
@@ -1,5 +1,22 @@
 <template>
-    <div><span>Finish Page {{results}}</span></div>
+    <div class="finish">
+      <div class="left-block">
+        <div class="home">
+          <font-awesome-icon icon="home" class="icons" size="lg"/>
+        </div>
+        <div class="result">
+          <div class="result-1">{{results.smile}}</div>
+          <div class="result-2">{{results.sad}}</div>
+          <div class="result-3">{{results.heart}}</div>
+        </div>
+        <div class="result-text"></div>
+        <div class="try"></div>
+      </div>
+      <div class="circle-pic">
+        <img src="../assets/image.jpg" alt="">
+      </div>
+    </div>
+
 </template>
 
 <script>
@@ -14,12 +31,41 @@ props: ['results'],
 }
 </script>
 
-<style>
+<style scoped>
 html, body{
   height: 100% ;
   width: 100%;
   margin: 0;
   overflow: hidden;
 }
+img{
+  position: absolute;
+  top: 0;
+  right: 30px;
+  height: 450px;
+}
+.result{
+  position: absolute;
+  top: 260px;
+  left: 130px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
 
+.home{
+  position: absolute;
+  top: 70px;
+  left: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  background: linear-gradient(63.53deg, #2D8550 16.62%, #5E6EC2 83.38%);
+  border-radius: 100px;
+}
+.icons{
+  align-self: center;
+}
 </style>
